38b76edc574a795587828489269c881f68f069a4
emeert <elijah.meert@proton.me>
2026-01-07 22:05:29 -0500

    More lessons today

---
+++ LearnPythonProgramingMasterClass/S3/S322.py
@@ -0,0 +1,23 @@
+print("Today is a good day to learn Python")
+print("Python is fun")
+print("Python's strings are easy to use")
+print('We can even include "qoutes" in strings')
+greeting = "Hello"
+name = "Gary"
+
+print(greeting + name)
+#if we want a space we can add that too. 
+print (greeting+' '+name)
+
+age = 24 
+
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+
+print(age)
+print(type(age))
+
---
+++ LearnPythonProgramingMasterClass/S3/S324.py
@@ -0,0 +1,24 @@
+print("Today is a good day to learn Python")
+print("Python is fun")
+print("Python's strings are easy to use")
+print('We can even include "qoutes" in strings')
+greeting = "Hello"
+name = "Gary"
+
+print(greeting + name)
+#if we want a space we can add that too. 
+print (greeting+' '+name)
+
+age = 24 
+
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+
+print(age_in_words)
+print(name + " is "+ age + "years old")
+print(type(age_in_words))
+
